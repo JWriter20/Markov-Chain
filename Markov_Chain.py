e01c6cb155ad8543118504c35fe9e5c87d7172fd
Jake <jwriter20@kentdenver.org>
2020-02-02 14:46:35 -0700

    Fixed a couple of bugs and made it so my story prints to an external file. Also reads in the files now instead of hard coding them

--- Markov_Chain.py
+++ Markov_Chain.py
@@ -1,14 +1,18 @@
 import random
 
-def findWords():
+def findWords(file):
     dict = {}
     startWords = []
-    file = open("TextFile.txt", "r")
+    file = open(file + ".txt", "r")
     allWords = file.read().split(" ")
+
     for i in range(len(allWords)-1):
+
         if allWords[i] in dict:
             if '.' in allWords[i]:
-                startWords.append(allWords[i+1])
+                if "." not in allWords[i+1]:
+                    if "\"\n" not in allWords[i+1]:
+                        startWords.append(allWords[i+1])
             dict.get(allWords[i]).append(allWords[i+1])
         else:
             dict[allWords[i]] = [allWords[i+1]]
@@ -16,16 +20,17 @@ def findWords():
     return dict
 
 def generate(dict, file, numWords):
-    newFile = open(file, "w+")
+    newFile = open(file + ".txt", "w+")
     output = ""
     count = 0
     currWord = random.choice(list(dict["StartWords"]))
-    while count < numWords:
+    while count < int(numWords):
         if currWord not in dict.keys():
             currWord = random.choice(list(dict.keys()))
         if '.' in currWord:
             next = random.choice(list(dict["StartWords"]))
-        next = random.choice(dict[currWord])
+        else:
+            next = random.choice(dict[currWord])
         output += (currWord + " ")
         currWord = next
         count += 1
--- testing.py
+++ testing.py
@@ -1,5 +1,9 @@
 import Markov_Chain
 
 if __name__ == '__main__':
-    dict = Markov_Chain.findWords()
-    Markov_Chain.generate(dict, "newStory.txt", 1000)
+    trainingFile = input("Please enter the name of the file you want to train the algorithm on: ")
+    numWords = input("Please enter the number of words you want your new story to be: ")
+    fileName = input("please input the name of the file on which you want to create your story: ")
+    dict = Markov_Chain.findWords(trainingFile)
+    Markov_Chain.generate(dict, fileName, numWords)
+
