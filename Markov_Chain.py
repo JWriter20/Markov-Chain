71a50ea66c96b4e3ce113da3cac2d8d6de902486
Jake <jwriter20@kentdenver.org>
2020-02-03 13:47:21 -0700

    added all styling and headers, fixed minor bugs, and added formatting

--- Markov_Chain.py
+++ Markov_Chain.py
@@ -1,14 +1,28 @@
 import random
+import re
+"""
+Author: Jake Writer
+Project description: This program provides the methods to be used when creating a Markov Chain. FindWords will create a
+dictionary of word keys and values using the inputted file and generate will use that dictionary, and create a 
+new text doc containing a story which uses the words from the initial file. This new file will have the inputted 
+number of words
+Date: 2/3/2020
+"""
 
+"""
+Name of Method: findWords 
+Argument types: String file
+purpose: To create a dictionary with key value pairs from the inputted file
+Assumptions: File that is taken in exists and is in the same folder
+"""
 def findWords(file):
     dict = {}
     startWords = []
     file = open(file + ".txt", "r")
-    allWords = file.read().split(" ")
+    allWords = file.read().replace("\n", " ").split()
 
     for i in range(len(allWords)-1):
-
-        if allWords[i] in dict:
+        if allWords[i] in dict.keys():
             if '.' in allWords[i]:
                 if "." not in allWords[i+1]:
                     if "\"\n" not in allWords[i+1]:
@@ -19,20 +33,39 @@ def findWords(file):
     dict["StartWords"] = startWords
     return dict
 
+
+"""
+Name of Method: Generate 
+Argument types: Dictionary dict, String file (will automatically add the .txt), int numWords
+purpose: To create a new story using the dictionary and output it to a new doc.
+Assumptions: dict exists and numWords is greater than or equal to 0, if you write to an existing file
+it will override it
+"""
 def generate(dict, file, numWords):
     newFile = open(file + ".txt", "w+")
     output = ""
     count = 0
     currWord = random.choice(list(dict["StartWords"]))
     while count < int(numWords):
+        willOutput = False
+        willOutput2 = False
+        if (count+1) % 20 == 0:
+            willOutput2 = True
         if currWord not in dict.keys():
             currWord = random.choice(list(dict.keys()))
         if '.' in currWord:
             next = random.choice(list(dict["StartWords"]))
+            randNum = random.randint(0, 4)
+            if randNum is 1:
+                willOutput = True
         else:
             next = random.choice(dict[currWord])
         output += (currWord + " ")
         currWord = next
+        if willOutput2:
+            output += "\n"
+        if willOutput:
+            output += "\n\n"
         count += 1
     newFile.write(output)
     return
--- testing.py
+++ testing.py
@@ -1,5 +1,10 @@
 import Markov_Chain
-
+"""
+Author: Jake Writer
+Project description: This is the main file that takes in the inputs and uses the methods from Markov_Chain to 
+create a new doc and output the new story to it.
+Date: 2/3/2020
+"""
 if __name__ == '__main__':
     trainingFile = input("Please enter the name of the file you want to train the algorithm on: ")
     numWords = input("Please enter the number of words you want your new story to be: ")
